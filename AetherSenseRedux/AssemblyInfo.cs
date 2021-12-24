4d2184a5d7ad715930d360ef60ee2b0f061fd033
Tama Gotchi <47679051+digital-pet@users.noreply.github.com>
2021-12-24 17:51:33 -0500

    Fixing build issues by nuking and rebuilding the csproj and related files

---
+++ AetherSenseRedux/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("Tama Gotchi (digital-pet)")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("All Rights Reserved")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Buttplugs on the moon")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.6.*")]
+[assembly: System.Reflection.AssemblyProductAttribute("AetherSenseRedux")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AetherSenseRedux")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.6.*")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
