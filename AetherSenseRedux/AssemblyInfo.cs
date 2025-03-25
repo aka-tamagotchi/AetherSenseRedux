270b2f1e070fa405bb69114b0cbb350f56d7497c
Cool Bug <psilidae@outlook.com>
2025-03-25 01:27:09 -0400

    build: revert a9800345, re-add AssemblyInfo

---
+++ AetherSenseRedux/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("Tama Gotchi (digital-pet)")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("All Rights Reserved")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Buttplugs on the moon")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.7 BETA")]
+[assembly: System.Reflection.AssemblyProductAttribute("AetherSenseRedux")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AetherSenseRedux")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.7.*")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows10.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows10.0")]
